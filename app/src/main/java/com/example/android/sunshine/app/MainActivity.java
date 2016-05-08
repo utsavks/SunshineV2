0e61770e405884f6a309dd3426b6f8c17a07f862
utsavks <uks.invincible@gmail.com>
2016-05-08 07:15:40 +0530

    Finish with lesson 1

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -8,6 +8,12 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -21,6 +27,7 @@ public class MainActivity extends ActionBarActivity {
                     .add(R.id.container, new PlaceholderFragment())
                     .commit();
         }
+
     }
 
     @Override
@@ -56,8 +63,19 @@ public class MainActivity extends ActionBarActivity {
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
+            String[] forecastArray ={"Today - Sunny - 88/63","Tomorrow - Foggy - 70/46",
+                    "Weds - Cloudy - 72/63","Thurs - Rainy - 64/51",
+                    "Fri - Foggy - 70/46","Sat - Sunny - 76/68"};
+            List<String> weekForecast = new ArrayList<>(Arrays.asList(forecastArray));
+            ListAdapter forecastAdapter = new ArrayAdapter<>(getActivity(),R.layout.list_item_forecast,R.id.list_item_forecast_textview,weekForecast);
+
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            ListView listview_forecast = (ListView)rootView.findViewById(R.id.listview_forecast);
+            listview_forecast.setAdapter(forecastAdapter);
+
             return rootView;
+
+
         }
     }
 }
